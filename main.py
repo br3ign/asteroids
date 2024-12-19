96007d44f768a8377499dd691ea757527d10085a
br3ign <alexsamm231@hotmail.com>
2024-12-18 22:00:43 -0400

    updated main.py and added delta time to ensure fps remains at 60

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ from contants import *
 
 def main():
     pygame.init()
+    game_clock = pygame.time.Clock()
+    dt = 0
+
     print("Starting asteroids!")
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Asteroids")
 
     while True:
         for event in pygame.event.get():
@@ -15,6 +19,8 @@ def main():
                 return
         screen.fill((0,0,0))
         pygame.display.flip()
+        dt = game_clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
