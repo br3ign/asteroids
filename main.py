0f32722e86ec755f454e18b27646810c6a0a6d93
br3ign <alexsamm231@hotmail.com>
2024-12-19 10:45:12 -0400

    updated main.py with 2 groups: updatable and drawable. added player class to groups and refactored the main loop

--- main.py
+++ main.py
@@ -1,28 +1,39 @@
 import pygame
 from contants import *
-import player
+import player as Player
 
+pygame.init()
 
 def main():
-    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     game_clock = pygame.time.Clock()
     dt = 0
 
-    player1 = player.Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    # groups
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.Player.containers = (updateable, drawable)
+
+    player = Player.Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     print("Starting asteroids!")
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
-
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    pygame.display.set_caption("Asteroids")
+    
+    pygame.display.set_caption("Asteroids")   
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("Black")
-        player1.update(dt)
-        player1.draw(screen)
+
+        for y in drawable:
+            y.draw(screen)
+        
+        for x in updateable:
+            x.update(dt)
         
         pygame.display.flip()
         dt = game_clock.tick(60) / 1000
