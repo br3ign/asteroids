ff07c291d8f6cf8cf07a81c3192d732727e4afad
br3ign <alexsamm231@hotmail.com>
2024-12-18 22:29:01 -0400

    add player.py, updated main.py with player class and update method. add new constants

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pygame
 from contants import *
+import player
+
 
 def main():
     pygame.init()
     game_clock = pygame.time.Clock()
     dt = 0
 
+    player1 = player.Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     print("Starting asteroids!")
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
@@ -17,7 +20,10 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0))
+        screen.fill("Black")
+        player1.update(dt)
+        player1.draw(screen)
+        
         pygame.display.flip()
         dt = game_clock.tick(60) / 1000
 
